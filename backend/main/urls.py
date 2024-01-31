1d2305d5e4f3225cebc1b190d928a3ed195b5a60
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-02-01 02:39:28 +0300

    added viewsets urls

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+from main.api import views as v
 
+router = SimpleRouter()
+router.register("town", v.TownViewSet)
+router.register("suburb", v.SuburbViewSet)
+router.register("house", v.HouseViewSet)
+router.register("agent", v.AgentViewSet)
+router.register("user", v.UserViewSet)
 
-urlpatterns = [
-
-]
+urlpatterns = router.urls
