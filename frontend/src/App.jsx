424d43e9777f05801d4f16dad97093de75069f2d
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-02-12 10:03:19 +0300

    semistandard issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import './App.css';
-import { createBrowserRouter, Outlet } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { LAYOUT } from './components/base_imports.jsx';
-import Home from './components/home/home.jsx';
+import './App.css'
+import { createBrowserRouter, Outlet } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { LAYOUT } from './components/base_imports.jsx'
+import Home from './components/home/home.jsx'
 
-const { Container, Spinner } = LAYOUT;
+const { Container, Spinner } = LAYOUT
 
 function Layout () {
   return (
     <Container fluid>
       <Outlet />
     </Container>
-  );
+  )
 }
 const router = createBrowserRouter([
   {
@@ -21,13 +21,13 @@ const router = createBrowserRouter([
       { path: '', element: <Home /> }
     ]
   }
-]);
-export default router;
+])
+export default router
 
 export function LoadingScreen () {
   return (
     <div className='vh-100 d-flex justify-content-center align-items-center'>
       <Spinner animation='border' variant='info' />
     </div>
-  );
+  )
 }
