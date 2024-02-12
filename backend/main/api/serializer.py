0e8e5736294e21b37202605ded049470c93dfc64
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-02-12 10:02:12 +0300

    fixed house serializer

--- backend/main/api/serializer.py
+++ backend/main/api/serializer.py
@@ -2,35 +2,36 @@ from rest_framework import serializers as sz
 from main import models as mdl
 
 
-
 class TownSerializer(sz.ModelSerializer):
-	class Meta:
-		model = mdl.Town
-		fields = "__all__"
+    class Meta:
+        model = mdl.Town
+        fields = "__all__"
 
 
 class SuburbSerializer(sz.ModelSerializer):
-	town = TownSerializer()
-	class Meta:
-		model = mdl.Suburb
-		fields = "__all__"
+    town = TownSerializer()
+
+    class Meta:
+        model = mdl.Suburb
+        fields = "__all__"
 
 
 class AgentSerializer(sz.ModelSerializer):
-	class Meta:
-		model = mdl.Agent
-		fields = "__all__"
+    class Meta:
+        model = mdl.Agent
+        fields = "__all__"
 
 
 class HouseSerializer(sz.ModelSerializer):
-	location = SuburbSerializer
-	Agent = AgentSerializer
-	class Meta:
-		model = mdl.House
-		fields = "__all__"
+    location = SuburbSerializer()
+    agent = AgentSerializer()
+
+    class Meta:
+        model = mdl.House
+        fields = "__all__"
 
 
 class UserSerializer(sz.ModelSerializer):
-	class Meta:
-		model = mdl.User
-		fields = "__all__"
+    class Meta:
+        model = mdl.User
+        fields = "__all__"
