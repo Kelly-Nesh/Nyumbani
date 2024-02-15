e31574bfbdf8dfb63017cfcea783359ba2b7b46c
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-02-15 11:41:11 +0300

    renamed to getdata

--- frontend/src/services/hooks/useGetHouses.jsx
+++
@@ -1,8 +0,0 @@
-import { getHouses } from "../api/api";
-import { useQuery } from "react-query";
-
-function useGetHouse() {
-  return useQuery({ queryKey: ["houses"], queryFn: getHouses });
-}
-
-export default useGetHouse;
